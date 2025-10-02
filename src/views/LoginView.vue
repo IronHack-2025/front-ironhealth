2867c00ad50f36be87659c22692d4ed41a4d8eef
alenorgue <alenoriegag@gmail.com>
2025-10-02 13:33:48 +0200

    Login-bg and LoginUX fix

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,5 +3,19 @@ import UserLogin from '@/components/UserLogin.vue'
 </script>
 
 <template>
-  <UserLogin />
+     <v-container class="LoginView" fluid>
+   <UserLogin />
+</v-container>
 </template>
+<style scoped>
+.LoginView {
+  background-image: url('@/assets/login-bg.jpg'); /* Color de fondo claro */
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  min-height: 100vh; /* Asegura que ocupe toda la altura de la ventana */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
